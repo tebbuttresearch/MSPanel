b4fcfa2e8c805a298f622dc42dfabe2f248332b9
TebbuttResearch <tim@tebbuttresearch.com>
2017-03-20 10:54:35 +1000

    2017-03-20b

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello world!"
 
-if __name__ = "__main__":
-    app.run()
+@app.route('/admin/')
+def hello_admin():
+    return 'Hello Admin'
+
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):
+    return 'Hello %s as Guest' % guest
+
+
+@app.route('/user/<name>')
+def hello_user(name):
+    if name=='admin':
+        return redirect(url_for('hello_admin'))
+    else:
+        return redirect(url_for('hello_guest',guest=name))
+
+
+@app.route('/version/')
+def revision():
+    return 'Version 0.1'
+
+if __name__ == '__main__':
+    app.run(debug=True)
